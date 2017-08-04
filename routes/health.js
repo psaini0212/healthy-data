19a2082b4487400b8033590aef18aa1afb279402
Pankaj Saini (Digital) <pankaj.saini@lloydsbanking.com>
2017-08-04 17:10:31 +0530

    serve health data in response

--- routes/health.js
+++ routes/health.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var healthData = require('../data/response.json');
+
 /* GET health data. */
 router.get('/', function(req, res, next) {
-  res.json({ status: 'health' });
+  res.json(healthData);
 });
 
 module.exports = router;
